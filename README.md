f87961e63baeaa62d999e77f3bcee66d7933a077
papatangosierra <pts@sockdolager.net>
2024-10-09 16:59:09 -0400

    participant animation

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Open http://localhost:3000/ in your browser to see the result.
 - Time Together
 - Solidarity
 - Parallel Dispatch Console Mk. I
-- Chronoencabulatorde
+- Chronoencabulator
 - Countdown Coop
 - Double Time
 - Multitime
--- frontend/src/User.tsx
+++ frontend/src/User.tsx
@@ -6,35 +6,50 @@ interface User {
 }
 
 const User: React.FC<User> = ({ id, name }) => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({
+    x: Math.random() * (window.innerWidth * 0.9),
+    y: Math.random() * (window.innerHeight * 0.9),
+  });
 
   useEffect(() => {
     // Initial random position
-    setPosition({
-      x: Math.random() * (window.innerWidth * 0.9),
-      y: Math.random() * (window.innerHeight * 0.9),
-    });
+    // setPosition({
+    //   x: Math.random() * (window.innerWidth * 0.9),
+    //   y: Math.random() * (window.innerHeight * 0.9),
+    // });
 
     // Set up interval for wandering movement
     const wanderInterval = setInterval(() => {
-      setPosition(prevPos => ({
-        x: Math.max(0, Math.min(window.innerWidth * 0.9, prevPos.x + (Math.random() - 0.5) * 25)),
-        y: Math.max(0, Math.min(window.innerHeight * 0.9, prevPos.y + (Math.random() - 0.5) * 25)),
+      setPosition((prevPos) => ({
+        x: Math.max(
+          0,
+          Math.min(
+            window.innerWidth * 0.9,
+            prevPos.x + (Math.random() - 0.5) * 10,
+          ),
+        ),
+        y: Math.max(
+          0,
+          Math.min(
+            window.innerHeight * 0.9,
+            prevPos.y + (Math.random() - 0.5) * 10,
+          ),
+        ),
       }));
-    }, 300); // Move every 2 seconds
+    }, 200); // Move every 2 seconds
 
     // Clean up interval on component unmount
     return () => clearInterval(wanderInterval);
   }, []);
 
   return (
-    <li 
-      className="user-container" 
+    <li
+      className="user-container"
       style={{
         position: "absolute",
         top: position.y,
         left: position.x,
-        transition: "all .3s linear",
+        transition: "all .2s linear",
       }}
     >
       {name}
