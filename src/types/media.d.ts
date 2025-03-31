ec4206adf496bebf61641110eaa1ed150d934263
papatangosierra <pts@sockdolager.net>
2025-03-31 13:56:24 -0400

    module declarations for asset inclusion

---
+++ src/types/media.d.ts
@@ -0,0 +1,19 @@
+declare module "*.mp3" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.png" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.css" {
+    const content: string;
+    export default content;
+}
+
+declare module "*.html" {
+    const content: string;
+    export default content;
+} 
